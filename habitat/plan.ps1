85f9d60805f58e2a8ef53f872c134473346d3d5f
Stephen Lauck <stephen.lauck@gmailcom>
2018-09-06 10:41:40 -0400

    build package

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -3,19 +3,17 @@ $pkg_origin="lauck"
 $pkg_version="01.10.2"
 $pkg_maintainer="Stephen Lauck <lauck@chef.io>"
 $pkg_license=@("GPL")
-$pkg_source="https://osdn.net/frs/redir.php?m=gigenet&f=%2Fstorage%2Fg%2Ft%2Fto%2Ftortoisesvn%2F1.10.1%2FApplication%2FTortoiseSVN-1.10.1.28295-x64-svn-1.10.2.msi"
+$pkg_source="https://downloads.sourceforge.net/project/tortoisesvn/1.9.7/Application/TortoiseSVN-1.9.7.27907-x64-svn-1.9.7.msi"
 $pkg_build_deps=@("core/lessmsi")
-$pkg_filename=@("TortoiseSVN-1.10.1.28295-x64-svn-1.10.2.msi")
+$pkg_shasum=@("a0616034862a0deb79b0601cb724c6e8e5859f9ee5e3a057d42bf3c723241396")
+$pkg_fileame=@("TortoiseSVN-1.9.7.27907-x64-svn-1.9.7.msi")
 
 function Invoke-Unpack {
   lessmsi x (Resolve-Path "$HAB_CACHE_SRC_PATH/$pkg_filename").Path
   mkdir "$HAB_CACHE_SRC_PATH/$pkg_dirname"
-  # Move-Item "" "$HAB_CACHE_SRC_PATH/$pkg_dirname"
-
-  # Remove-Item -Recurse -Force .\AspNetMVC1
+  Move-Item "TortoiseSVN-1.9.7.27907-x64-svn-1.9.7\SourceDir\Program Files\TortoiseSVN\" "$HAB_CACHE_SRC_PATH/$pkg_dirname"
 }
 
 function Invoke-Install {
-  return 0
-  # Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_dirname/ASP.NET MVC 1.0/*" "$pkg_prefix" -Recurse -Force
+  Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_dirname/bin/*" "$pkg_prefix" -Recurse -Force
 }
